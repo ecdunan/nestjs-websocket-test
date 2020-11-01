bf3bfa95c0ec0ebf888933bb4073013fbe1c3373
Emmanuel Dunan <ecdunan@gmail.com>
2020-11-01 14:46:28 +0800

    Initial implementation of gateway

---
+++ src/app.gateway.ts
@@ -0,0 +1,31 @@
+import { Logger } from '@nestjs/common';
+import {
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsResponse,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway()
+export class AppGateway implements OnGatewayInit {
+  constructor(private logger: Logger) {
+    this.logger = new Logger('AppGateway');
+  }
+
+  @WebSocketServer() server: Server;
+
+  afterInit(server: Server) {
+    this.logger.log('AppGateway initialized');
+    server.emit('Web socket server initialized');
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(client: Socket, payload: string): WsResponse<string> {
+    return {
+      event: 'messageFromServer',
+      data: payload,
+    };
+  }
+}
